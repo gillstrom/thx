5d8f21032de5b0c4fa2315bcdcd4cf2c7c0e9641
Federico Brigante <bfred-it@users.noreply.github.com>
2016-04-24 02:58:18 -0700

    Properly get unique expression from list

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 var list = require('./list.json');
 var uniqueRandomArray = require('unique-random-array');
+var getUniqueFromList = uniqueRandomArray(list);
 
 module.exports = function () {
-	return uniqueRandomArray(list)();
+	return getUniqueFromList();
 };
 
 module.exports.all = list;
