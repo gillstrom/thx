92d3fe3383e22fd52f120447801ed960c3e96fe3
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-24 12:29:53 +0200

    Adjust tests to newer AVA version

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import test from 'ava';
 import fn from './';
 
-test('should return an array', t => {
+test.cb('should return an array', t => {
 	t.is(Array.isArray(fn.all), true);
 	t.end();
 });
 
-test('should return a random string from the list', t => {
+test.cb('should return a random string from the list', t => {
 	t.is(typeof fn(), 'string');
 	t.is(fn.all.indexOf(fn()) !== -1, true);
 	t.end();
